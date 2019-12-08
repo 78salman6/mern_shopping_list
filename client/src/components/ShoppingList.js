a949bec2929dcc416e1a65160daf868e8db4f022
Kashif <212773644@logon.ds.ge.com>
2019-12-09 00:50:55 +0530

    Done the redux part ( added actions, reducers and containers)

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,37 +1,25 @@
 import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import uuid from 'uuid';
 import { stat } from 'fs';
+import { connect } from 'react-redux';
+import { getItems, deleteItem } from '../actions/itemActions';
+import PropTypes from 'prop-types';
 
 class ShoppingList extends Component {
-    state = {
-        items: [
-            { id: uuid(), name: 'Eggs' },
-            { id: uuid(), name: 'Butter' },
-            { id: uuid(), name: 'Milk' },
-            { id: uuid(), name: 'Water' }
 
-        ]
+    componentDidMount() {
+        this.props.getItems();
+    }
+
+    onDeleteClick = (id) => {
+        this.props.deleteItem(id);
     }
 
     render() {
-        const { items } = this.state;
+        const { items } = this.props.item;
         return(
             <Container>
-                <Button
-                    color="dark"
-                    style={{marginBottom: '2rem'}}
-                    onClick={() => {
-                        const name = prompt('Enter Item');
-                        if(name) {
-                            this.setState(state => ({
-                                items: [...state.items, { id: uuid(), name }]
-                            }));
-                        }
-                    }}
-                >Add Item
-                </Button>
 
                 <ListGroup>
                     <TransitionGroup className="shopping-list">
@@ -42,11 +30,7 @@ class ShoppingList extends Component {
                                         className="remove-btn"
                                         color="danger"
                                         size="sm"
-                                        onClick={() => {
-                                            this.setState(state => ({
-                                                items: state.items.filter(item => item.id !== id)
-                                            }));
-                                        }}
+                                        onClick={this.onDeleteClick.bind(this, id)}
                                     >&times;
                                     </Button>
                                     {name}
@@ -60,4 +44,16 @@ class ShoppingList extends Component {
     }
 }
 
-export default ShoppingList;
+ShoppingList.propTypes = {
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+    item: state.item
+});
+
+export default connect(
+    mapStateToProps, 
+    { getItems, deleteItem }
+)(ShoppingList);
---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,35 @@
+import uuid from 'uuid';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
+import { stat } from 'fs';
+
+const initialState = {
+    items: [
+        { id: uuid(), name: 'Eggs' },
+        { id: uuid(), name: 'Butter' },
+        { id: uuid(), name: 'Milk' },
+        { id: uuid(), name: 'Meat' }
+
+    ]
+}
+
+export default function(state = initialState, action) {
+    switch (action.type) {
+        case GET_ITEMS:
+            return {
+                ...state
+            }
+        case DELETE_ITEM:
+            return {
+                ...state,
+                items: state.items.filter(item => item.id !== action.payload)
+            }
+        case ADD_ITEM:
+        return {
+            ...state,
+            items: [action.payload, ...state.items]
+        }
+        default:
+            return state;
+
+    }
+}
